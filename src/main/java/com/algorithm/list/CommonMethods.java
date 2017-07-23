0007284e8977f41410da97472226c933fa6cbe39
vitaly.gerasev <vitaly.gerasev@reltio.com>
2017-07-23 23:18:31 +0500

    added more tests and fixed bug in list.isOneContainsTwo

--- src/main/java/com/algorithm/list/CommonMethods.java
+++ src/main/java/com/algorithm/list/CommonMethods.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class CommonMethods {
     public <B extends Comparable<B> , A extends B> boolean isOneContainsTwo  (List<A> bigOne, List<B> smallTwo) {
         boolean bResult = true;
-        if (bigOne==null || smallTwo==null || smallTwo.size()==0) {
+        if (bigOne==null || smallTwo==null || (smallTwo.size() == 0 && bigOne.size() > 0)) {
             return false;
         }
         if (bigOne.size() == 0 && smallTwo.size() == 0) return true;
