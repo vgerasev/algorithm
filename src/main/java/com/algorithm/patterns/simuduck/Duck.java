f88761a30e6fb4a8993b8df2e1b726680dd60a9f
vitaly.gerasev <vitaly.gerasev@reltio.com>
2017-08-27 19:50:05 +0500

    added pattern Strategy

--- src/main/java/com/algorithm/patterns/simuduck/Duck.java
+++ src/main/java/com/algorithm/patterns/simuduck/Duck.java
@@ -11,6 +11,14 @@ public abstract class Duck {
 
     public abstract String display();
 
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+
     public String performFly() {
         return flyBehavior.fly();
     }
--- src/test/java/com/algorithm/tests/patterns/simuduck/DuckTest.java
+++ src/test/java/com/algorithm/tests/patterns/simuduck/DuckTest.java
@@ -2,7 +2,9 @@ package com.algorithm.tests.patterns.simuduck;
 
 import com.algorithm.patterns.contstants.Constants;
 import com.algorithm.patterns.simuduck.Duck;
+import com.algorithm.patterns.simuduck.FlyRocketPowered;
 import com.algorithm.patterns.simuduck.MallardDuck;
+import com.algorithm.patterns.simuduck.ModelDuck;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,5 +16,17 @@ public class DuckTest {
         Assert.assertTrue(mallard.performFly().contains(Constants.FLY_WING));
         Assert.assertTrue(mallard.performQuack().contains(Constants.QUACK));
         Assert.assertTrue(mallard.swim().contains(Constants.SWIM));
+    }
+
+    @Test
+    public void modelDuckOneTest() {
+        Duck model = new ModelDuck();
+        Assert.assertTrue(model.display().contains(Constants.MODEL_DISPLAY));
+        Assert.assertTrue(model.performFly().contains(Constants.FLY_NO_WAY));
+        Assert.assertTrue(model.performQuack().contains(Constants.QUACK));
+        Assert.assertTrue(model.swim().contains(Constants.SWIM));
+
+        model.setFlyBehavior(new FlyRocketPowered());
+        Assert.assertTrue(model.performFly().contains(Constants.FLY_ROCKET));
     }
 }
